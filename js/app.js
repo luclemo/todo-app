40bb4566b1fb785534f89aec6a9a56c9beeb2eaa
Lucas Lemonnier <ll@ttwebco.com>
2015-04-07 16:38:31 -0400

    wrote speudo code

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Add a new task
+	// When button is pressed
+	// Create new li with text from #new-task:
+		// input (checkbox)
+		// label
+		// input (text)
+		// button.edit
+		// button.delete
+		// each elements, needs to be modified and appended
+
+// Edit an existing task
+	// When the edit button is pressed
+		// if the class of the parent is .editMode
+			// Switch from .editMode
+			// lable text becomes the input's value
+		// else
+			// switch to .editMode
+			// input value becomes the label's text
+
+		// toggle .editMode on parent
+
+
+// Delete an existing task
+	// When the delete button is pressed
+	 // remove the parent li from the ul
+
+
+// Mark task as complete
+	// When the checkbox is checked
+		// append the task li to the #completed-tasks
+
+
+// Mark task as incomplete
+	// When the checkbox is unchecked
+	// append the task li to the #incomplete-tasks
