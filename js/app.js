3c41901a205330912d6c1f778e4e2a2274d9da4c
Lucas Lemonnier <ll@ttwebco.com>
2015-04-07 16:58:02 -0400

    added variables and empty functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+var taskInput = document.getElementById('new-task'); // new-task
+var addButton = document.getElementsByTagName('button')[0]; // first button
+var incompleteTaskHolder = document.getElementById('incomplete-tasks') // incomplete-tasks
+var completedTaskHolder = document.getElementById('completed-tasks')// completed-tasks
+
 // Add a new task
+var addTask = function() {
 	// When button is pressed
 	// Create new li with text from #new-task:
 		// input (checkbox)
@@ -7,8 +13,10 @@
 		// button.edit
 		// button.delete
 		// each elements, needs to be modified and appended
+}
 
 // Edit an existing task
+var editTask = function() {
 	// When the edit button is pressed
 		// if the class of the parent is .editMode
 			// Switch from .editMode
@@ -18,18 +26,25 @@
 			// input value becomes the label's text
 
 		// toggle .editMode on parent
+}
 
 
 // Delete an existing task
+var deleteTask = function() {
 	// When the delete button is pressed
 	 // remove the parent li from the ul
+}
 
 
 // Mark task as complete
+var taskCompleted = function() {
 	// When the checkbox is checked
 		// append the task li to the #completed-tasks
+}
 
 
 // Mark task as incomplete
+var taskIncomplete = function() {
 	// When the checkbox is unchecked
 	// append the task li to the #incomplete-tasks
+}
